e9d4f79a79170effb43e3b9fa2fa036679f592f4
iga-yamaguchi <iganin2@gmail.com>
2018-09-09 00:27:24 +0900

    不要なinsert

--- webapp/go/src/isubata/app.go
+++ webapp/go/src/isubata/app.go
@@ -659,10 +659,10 @@ func postProfile(c echo.Context) error {
 	}
 
 	if avatarName != "" && len(avatarData) > 0 {
-		_, err := db.Exec("INSERT INTO image (name, data) VALUES (?, ?)", avatarName, avatarData)
-		if err != nil {
-			return err
-		}
+		/*		_, err := db.Exec("INSERT INTO image (name, data) VALUES (?, ?)", avatarName, avatarData)
+				if err != nil {
+					return err
+				}*/
 		_, err = db.Exec("UPDATE user SET avatar_icon = ? WHERE id = ?", avatarName, self.ID)
 		if err != nil {
 			return err
@@ -678,32 +678,6 @@ func postProfile(c echo.Context) error {
 
 	return c.Redirect(http.StatusSeeOther, "/")
 }
-
-//func getIcon(c echo.Context) error {
-//	var name string
-//	var data []byte
-//	err := db.QueryRow("SELECT name, data FROM image WHERE name = ?",
-//		c.Param("file_name")).Scan(&name, &data)
-//	if err == sql.ErrNoRows {
-//		return echo.ErrNotFound
-//	}
-//	if err != nil {
-//		return err
-//	}
-//
-//	mime := ""
-//	switch true {
-//	case strings.HasSuffix(name, ".jpg"), strings.HasSuffix(name, ".jpeg"):
-//		mime = "image/jpeg"
-//	case strings.HasSuffix(name, ".png"):
-//		mime = "image/png"
-//	case strings.HasSuffix(name, ".gif"):
-//		mime = "image/gif"
-//	default:
-//		return echo.ErrNotFound
-//	}
-//	return c.Blob(http.StatusOK, mime, data)
-//}
 
 func tAdd(a, b int64) int64 {
 	return a + b
